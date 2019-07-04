c540076dc6a1cf0afe1a1f589310259ac899ef4f
melekpro <melekpro6@gmail.com>
2019-07-04 18:22:51 +0300

    Build

--- build.sh
+++ build.sh
@@ -1,137 +1,223 @@
 #!/bin/bash
-##
-#  Copyright (C) 2015, Samsung Electronics, Co., Ltd.
-#  Written by System S/W Group, S/W Platform R&D Team,
-#  Mobile Communication Division.
 #
-#  Edited by Diep Quynh Nguyen (diepquynh1501)
-##
-
-set -e -o pipefail
-
-DEFCONFIG=mt6737t-grandpplte_defconfig
-NAME=RZ_kernel
-VERSION=v1.0
-
-export ARCH=arm
-export LOCALVERSION=-${VERSION}
-
-KERNEL_PATH=$(pwd)
-KERNEL_ZIP=${KERNEL_PATH}/kernel_zip
-KERNEL_ZIP_NAME=${NAME}_${VERSION}.zip
-KERNEL_IMAGE=${KERNEL_ZIP}/tools/zImage
-DT_IMG=${KERNEL_ZIP}/tools/dt.img
-MODULES_PATH=${KERNEL_ZIP}/system/lib/modules
-OUTPUT_PATH=${KERNEL_PATH}/output
-
-JOBS=`grep processor /proc/cpuinfo | wc -l`
-
+# Copyright (C) 2018 Yuvraj Saxena (frap130)
+# Copyright (C) 2018 SaMad SegMane (MrSaMaDo)
+#
+ 
+# Script To Compile Kernels
+ 
 # Colors
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
-
-function build() {
-	clear;
-
-	BUILD_START=$(date +"%s");
-	echo -e "$cyan"
-	echo "***********************************************";
-	echo "              Compiling RZ kernel          	     ";
-	echo -e "***********************************************$nocol";
-	echo -e "$red";
-
-	if [ ! -e ${OUTPUT_PATH} ]; then
-		mkdir ${OUTPUT_PATH};
-	fi;
-
-	if [ ! -e ${MODULES_PATH} ]; then
-		mkdir -p ${MODULES_PATH};
-	fi;
-
-	echo -e "Initializing defconfig...$nocol";
-	make O=output ${DEFCONFIG}
-	echo -e "$red";
-	echo -e "Building kernel...$nocol";
-	make O=output -j${JOBS};
-	#make O=output -j${JOBS} dtbs;
-	#gcc -o ${KERNEL_PATH}/scripts/dtbTool ${KERNEL_PATH}/scripts/dtbtool.c
-	#./scripts/dtbTool -o ${DT_IMG} -s 2048 $(pwd)/output/arch/arm/boot/
-	find ${KERNEL_PATH} -name "zImage" -exec mv -f {} ${KERNEL_ZIP}/tools \;
-	find ${KERNEL_PATH} -name "*.ko" -exec mv -f {} ${MODULES_PATH} \;
-
-	BUILD_END=$(date +"%s");
-	DIFF=$(($BUILD_END - $BUILD_START));
-	echo -e "$yellow";
-	echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol";
+ORNG=$'\033[0;33m'
+CYN=$'\033[0;36m'
+PURP=$'\033[0;35m'
+BLINK_RED=$'\033[05;31m'
+BLUE=$'\033[01;34m'
+BLD=$'\033[1m'
+GRN=$'\033[01;32m'
+RED=$'\033[01;31m'
+RST=$'\033[0m'
+YLW=$'\033[01;33m'
+ 
+function START_SCRIPT() {
+echo "${GRN}
+ 
+ __      __       .__                                ___________      ___________.__             __________      .__.__       .___   _________            .__        __   
+/  \    /  \ ____ |  |   ____  ____   _____   ____   \__    ___/___   \__    ___/|  |__   ____   \______   \__ __|__|  |    __| _/  /   _____/ ___________|__|______/  |_ 
+\   \/\/   // __ \|  | _/ ___\/  _ \ /     \_/ __ \    |    | /  _ \    |    |   |  |  \_/ __ \   |    |  _/  |  \  |  |   / __ |   \_____  \_/ ___\_  __ \  \____ \   __\
+ \        /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/    |    |(  <_> )   |    |   |   Y  \  ___/   |    |   \  |  /  |  |__/ /_/ |   /        \  \___|  | \/  |  |_> >  |  
+  \__/\  /  \___  >____/\___  >____/|__|_|  /\___  >   |____| \____/    |____|   |___|  /\___  >  |______  /____/|__|____/\____ |  /_______  /\___  >__|  |__|   __/|__|  
+       \/       \/          \/            \/     \/                                   \/     \/          \/                    \/          \/     \/         |__|         
+ 
+ ${RST}" 
+        HELP
+        read -rsn1 answer
+	case "${answer}" in
+                "b"|"build")
+                        BUILD=y
+                        ;;
+		"c"|"clean")
+			CLEAN=y
+			;;
+		*)			
+                        echo "${RED}Opps:Invalid Option${RST}"
+                        exit 1;
+			;;
+		esac
+		shift
 }
-
-function make_zip() {
-	echo -e "$red";
-	echo -e "Making flashable zip...$nocol";
-
-	cd ${KERNEL_PATH}/kernel_zip;
-	zip -r ${KERNEL_ZIP_NAME} ./;
-	mv ${KERNEL_ZIP_NAME} ${KERNEL_PATH};
+ 
+function BANNER() {
+echo "${BLINK_RED}        
+ 
+▀█████████▄   ▄█          ▄████████  ▄████████    ▄█   ▄█▄    ▄██████▄     ▄█    █▄     ▄██████▄     ▄████████     ███     
+  ███    ███ ███         ███    ███ ███    ███   ███ ▄███▀   ███    ███   ███    ███   ███    ███   ███    ███ ▀█████████▄ 
+  ███    ███ ███         ███    ███ ███    █▀    ███▐██▀     ███    █▀    ███    ███   ███    ███   ███    █▀     ▀███▀▀██ 
+ ▄███▄▄▄██▀  ███         ███    ███ ███         ▄█████▀     ▄███         ▄███▄▄▄▄███▄▄ ███    ███   ███            ███   ▀ 
+▀▀███▀▀▀██▄  ███       ▀███████████ ███        ▀▀█████▄    ▀▀███ ████▄  ▀▀███▀▀▀▀███▀  ███    ███ ▀███████████     ███     
+  ███    ██▄ ███         ███    ███ ███    █▄    ███▐██▄     ███    ███   ███    ███   ███    ███          ███     ███     
+  ███    ███ ███▌    ▄   ███    ███ ███    ███   ███ ▀███▄   ███    ███   ███    ███   ███    ███    ▄█    ███     ███     
+▄█████████▀  █████▄▄██   ███    █▀  ████████▀    ███   ▀█▀   ████████▀    ███    █▀     ▀██████▀   ▄████████▀     ▄████▀   
+             ▀                                   ▀                                             
+${RST} "
 }
-
-function rm_if_exist() {
-	if [ -e $1 ]; then
-		rm -rf $1;
-	fi;
+ 
+function BINFO() {
+        VERSION="V3"
+        BUILD_DATE=$(echo "$(date)" | sed 'y/ /-/')
+        export KBUILD_BUILD_USER=Melek
+        export KBUILD_BUILD_HOST=superkernel
 }
-
-function clean() {
-	echo -e "$red";
-	echo -e "Cleaning build environment...$nocol";
-	make -j${JOBS} mrproper;
-
-	rm_if_exist ${KERNEL_ZIP_NAME};
-	rm_if_exist ${OUTPUT_PATH};
-	rm_if_exist ${MODULES_PATH};
-	rm_if_exist ${DT_IMG};
-	rm_if_exist ${KERNEL_PATH}/scripts/dtbTool;
-
-	echo -e "$yellow";
-	echo -e "Done!$nocol";
+ 
+function TOOLCHAIN() {
+if [[ ! -d gcc ]]; then 
+    sudo rm -rf linaro
+#   sudo rm -rf gcc/.git
+    echo "${GRN} #####################################"
+    echo "${GRN} #                                   #"
+    echo "${GRN} #        Cloning TOOLCHAIN...       #"
+    echo "${GRN} #                                   #"
+    echo "${GRN} #####################################"
+    git clone -q https://github.com/Skyrimus/arm-eabi-4.8-google-mt6580
+    export ARCH=arm CROSS_COMPILE=${PWD}/arm-eabi-4.8-google-mt6580/bin/arm-eabi-
+    export SUBARCH=arm
+else
+    export ARCH=arm CROSS_COMPILE=${PWD}/arm-eabi-4.8-google-mt6580/bin/arm-eabi-
+    export SUBARCH=arm
+fi
 }
-
-function main() {
-	clear;
-	read -p "Please specify Toolchain path: " tcpath;
-	if [ "${tcpath}" == "" ]; then
-		echo -e "$red"
-		export CROSS_COMPILE=$(pwd)/linaro-6.2/bin/arm-eabi-;
-		echo -e "No toolchain path found. Using default local one:$nocol ${CROSS_COMPILE}";
-	else
-		export CROSS_COMPILE=${tcpath};
-		echo -e "$red";
-		echo -e "Specified toolchain path: $nocol ${CROSS_COMPILE}";
-	fi;
-
-	echo -e "*****************************************************************************";
-	echo "      RZ Kernel for Samsung Galaxy J2 Prime (Grand Prime Plus) SM-G532x";
-	echo -e "*****************************************************************************";
-	echo "Choices:";
-	echo "1. Cleanup source";
-	echo "2. Build kernel";
-	echo "3. Build kernel then make flashable ZIP";
-	echo "4. Make flashable ZIP package";
-	echo "Leave empty to exit this script (it'll show invalid choice)";
-
-	read -n 1 -p "Select your choice: " -s choice;
-	case ${choice} in
-		1) clean;;
-		2) build;;
-		3) build
-		   make_zip;;
-		4) make_zip;;
-		*) echo
-		   echo "Invalid choice entered. Exiting..."
-		   sleep 2;
-		   exit 1;;
-	esac
+ 
+function BUILD() {
+        mkdir -p out
+        echo "${PURP} #####################################"
+        echo "${PURP} #                                   #"
+        echo "${PURP} #        READING DEFCONFIG          #"
+        echo "${PURP} #                                   #"
+        echo "${PURP} #####################################"
+        make O=BGH-Out/ TARGET_ARCH=arm mt6737t-grandpplte_defconfig | tee -a defconfig.log
+        echo "${YLW} #####################################"
+        echo "${YLW} #                                   #"
+        echo "${YLW} #       REAL BUILDING STARTED       #"
+        echo "${YLW} #                                   #"
+        echo "${YLW} #####################################"
+        make -j$(nproc --all) O=BGH-Out/ TARGET_ARCH=arm | tee -a kernel.log
+        OIMAGE=BGH-Out/arch/arm/boot/zImage-dtb
 }
-
-main $@
+ 
+function CHECK() {
+if [[ ! -e ${OIMAGE} ]];
+then
+        echo "${RED} #####################################"
+        echo "${RED} #                                   #"
+        echo "${RED} #           BUILD ERROR             #"
+        echo "${RED} #                                   #"
+        echo "${RED} #####################################"
+        TRANSFER kernel.log
+else
+        BANNER
+        echo "${GRN} #####################################"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #    SUCCESSFULLY BUILDED KERNEL    #"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #####################################"
+        mv ${OIMAGE} zImage
+        zip -r9 BlackGHostKernel-${VERSION}-${BUILD_DATE}.zip zImage >> /dev/null
+	rm zImage
+        echo " "
+        echo "${GRN} #      Do You Want To Upload?       #"
+        echo " "
+        read -rsn1 option
+        [ "$option" == "y" ] && TRANSFER BlackGHostKernel-${VERSION}-${BUILD_DATE}.zip
+fi
+}
+ 
+function CLEAN() {
+        echo "${GRN} #####################################"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #     Cleaning Kernel Source..      #"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #####################################"
+	rm -rf *.log *.zip out BGH-Out
+        make clean >> /dev/null
+        make mrproper >> /dev/null
+clear
+}
+ 
+function HELP() {
+        echo "${GRN} #####################################"
+        echo "${GRN} #  BlackGHost Kernel Build Script   #"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #  b, build  >   Build The Kernel   #"
+        echo "${GRN} #  c, clean  >   Clean The Source   #"
+        echo "${GRN} #                                   #"
+        echo "${GRN} #####################################"
+}
+ 
+function TRANSFER() {
+        file="$1"
+        zipname=$(echo "${file}" | awk -F '/' '{print $NF}')
+        destination="$2"
+        url=$(curl -# -T "${file}" https://transfer.sh/${destination})
+        echo "Download $zipname at $url"
+}
+ 
+function FORMAT_TIME() {
+        MINS=$(((${1}-${2})/60))
+        SECS=$(((${1}-${2})%60))
+if [[ ${MINS} -ge 60 ]]; then
+        HOURS=$((${MINS}/60))
+        MINS=$((${MINS}%60))
+fi
+ 
+if [[ ${HOURS} -eq 1 ]]; then
+        TIME_STRING+="1 HOUR, "
+elif [[ ${HOURS} -ge 2 ]]; then
+        TIME_STRING+="${HOURS} HOURS, "
+fi
+ 
+if [[ ${MINS} -eq 1 ]]; then
+        TIME_STRING+="1 MINUTE"
+else
+        TIME_STRING+="${MINS} MINUTES"
+fi
+ 
+if [[ ${SECS} -eq 1 && -n ${HOURS} ]]; then
+        TIME_STRING+=", AND 1 SECOND"
+elif [[ ${SECS} -eq 1 && -z ${HOURS} ]]; then
+        TIME_STRING+=" AND 1 SECOND"
+elif [[ ${SECS} -ne 1 && -n ${HOURS} ]]; then
+        TIME_STRING+=", AND ${SECS} SECONDS"
+elif [[ ${SECS} -ne 1 && -z ${HOURS} ]]; then
+        TIME_STRING+=" AND ${SECS} SECONDS"
+fi
+ 
+        echo ${TIME_STRING}
+}
+ 
+        BANNER
+ 
+if [[ ${BUILDING_ON_CI} = 'y' ]]; then
+        START_SCRIPT "${@}"
+else
+	START_SCRIPT
+fi
+ 
+if [[ "${BUILD}" = 'y' ]]; then
+clear
+        START=$(date +"%s")
+        TOOLCHAIN
+clear
+        BINFO
+        sleep 0.3
+        BUILD
+        CHECK
+        END=$(date +%s)
+        TIME_STRING="$(FORMAT_TIME "${START}" "${END}")"
+        echo "${GRN}Completed In: ${TIME_STRING}"
+elif [[ "${CLEAN}" = 'y' ]]; then
+        CLEAN
+else
+if [[ "${HELP}" = 'y' ]]; then
+clear
+        HELP
+fi
+fi
